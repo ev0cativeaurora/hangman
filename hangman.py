ac5e3a29dee07f8895704d6303ae89f2a40e3ff0
ev0cativeaurora <s.anouar.edu@gmail.com>
2025-04-11 14:28:06 +0200

    Implement word() function for picking random word

--- hangman.py
+++ hangman.py
@@ -3,3 +3,18 @@ import random
 # Keep track of scores
 player_score = 0
 computer_score = 0
+
+
+# Select random word from file
+def word():
+    """
+    Picks a random word from 'words.txt'.
+    """
+    word_list = open("words.txt", "r").readlines()
+    # Use randint(0, len(word_list) - 1) or randrange for safety
+    random_number = random.randint(0, len(word_list) - 1)
+    chosen_word = word_list[random_number].rstrip()
+    # Convert to lowercase to match user guesses
+    chosen_word = chosen_word.lower()
+    return chosen_word
+
