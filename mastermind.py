a48cc23c9d33ca781b39081995b643defd9d47d4
ev0cativeaurora <s.anouar.edu@gmail.com>
2025-04-11 16:07:05 +0200

    Add mastermind.py with placeholder start() function

---
+++ mastermind.py
@@ -0,0 +1,8 @@
+# mastermind.py
+
+def start():
+    """
+    Placeholder for Mastermind game logic.
+    We'll adapt Hangman structure to Mastermind step by step.
+    """
+    print("Welcome to Mastermind (Coming soon...)")
