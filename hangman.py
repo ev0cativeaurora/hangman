1e9b2f5bb2041482868ee34ae05fb1c91958abf7
ev0cativeaurora <s.anouar.edu@gmail.com>
2025-04-11 14:29:40 +0200

    Add start() function with welcome message and loop

--- hangman.py
+++ hangman.py
@@ -90,3 +90,25 @@ def hanged(man):
     '''
     ]
     return graphic[man]
+
+
+# Game Menu 
+def start():
+    """
+    Starts the Hangman game session, shows welcome message and instructions,
+    and loops over new games until the player chooses to stop.
+    """
+    print("\nWelcome to Hangman!")
+    print("Instructions:")
+    print("1. You have 6 attempts to guess the word.")
+    print("2. One wrong guess removes one attempt.")
+    print("3. If you guess the same letter again, you'll be asked to pick another letter.")
+    print("4. Enjoy the game!\n")
+
+    # Start the main loop of the game
+    while game():
+        pass
+
+    # After exiting the loop, show final scores
+    print("")
+    scores()
